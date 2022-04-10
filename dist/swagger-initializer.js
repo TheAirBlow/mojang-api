b70296fc06dbcf69a3892966173d4da4712c0414
TheAirBlow <pilexet.danila@gmail.com>
2022-04-10 22:25:29 +0500

    Deleted Mojang Account API, it's only endpoint got deprecated

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -2,7 +2,6 @@ window.onload = function() {
   window.ui = SwaggerUIBundle({
     urls: [
       {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/mojang/api.yaml", "name": "Public Mojang API"},
-      {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/mojang/account.yaml", "name": "Mojang Account API"},
       {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/mojang/sessionserver.yaml", "name": "Mojang Session API"},
       {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/mojang/authserver.yaml", "name": "Yggdrasil Mojang Server"},
       {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/minecraft/api.yaml", "name": "Minecraft Services API"}
