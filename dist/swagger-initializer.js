c28ea3d771a222ba1b2b4e7ba78250964f9ed3e1
TheAirBlow <pilexet.danila@gmail.com>
2022-04-10 13:28:09 +0500

    Create stubs

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,12 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    urls: [{"url":"https://github.com/TheAirBlow/mojang-api/blob/master/swagger.yaml", "name":"Public Mojang API"}],
+    urls: [
+      {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/mojang/api.yaml", "name": "api.mojang.com"},
+      {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/mojang/account.yaml", "name": "account.mojang.com"},
+      {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/mojang/sessionserver.yaml", "name": "sessionserver.mojang.com"},
+      {"url": "https://raw.githubusercontent.com/TheAirBlow/mojang-api/master/docs/minecraft/api.yaml", "name": "api.minecraftservices.com"}
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
